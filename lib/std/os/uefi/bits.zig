089ead98fd8fecbf356e24296cdb4875a73f8b0b
Nameless <truemedian@gmail.com>
2024-09-09 15:29:50 -0500

    more work

--- lib/std/os/uefi/bits.zig
+++ lib/std/os/uefi/bits.zig
@@ -10,7 +10,34 @@ pub const EFIAPI: std.builtin.CallingConvention = switch (builtin.target.cpu.arc
     else => @compileError("UEFI does not support this architecture"),
 };
 
-pub const Guid = extern struct {};
+pub const Guid = extern struct {
+    data1: u32,
+    data2: u16,
+    data3: u16,
+    data4: [8]u8,
+
+    pub fn format(guid: Guid, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        _ = options;
+
+        if (comptime std.mem.eql(u8, fmt, "x") or fmt.len == 0) {
+            try writer.print("{x:0>8}-{x:0>4}-{x:0>4}-{}", .{
+                guid.data1,
+                guid.data2,
+                guid.data3,
+                std.fmt.fmtSliceHexLower(guid.data4),
+            });
+        } else if (comptime std.mem.eql(u8, fmt, "X")) {
+            try writer.print("{X:0>8}-{X:0>4}-{X:0>4}-{}", .{
+                guid.data1,
+                guid.data2,
+                guid.data3,
+                std.fmt.fmtSliceHexUpper(guid.data4),
+            });
+        } else {
+            @compileError("unsupported format string for Guid.format");
+        }
+    }
+};
 
 /// A collection of related interfaces.
 pub const Handle = *const opaque {};
@@ -19,6 +46,38 @@ pub const Handle = *const opaque {};
 pub const Event = *const opaque {};
 
 pub const LogicalBlockAddress = u64;
+pub const TimerDelay = enum(u32) {
+    cancel,
+    periodic,
+    relative,
+};
+
+pub const AllocateType = enum(u32) {
+    any,
+    max_address,
+    exact_address,
+};
+
+pub const MemoryType = enum(u32) {
+    reserved,
+    loader_code,
+    loader_data,
+    boot_services_code,
+    boot_services_data,
+    runtime_services_code,
+    runtime_services_data,
+    conventional,
+    unusable,
+    acpi_reclaim,
+    acpi_nvs,
+    memory_mapped_io,
+    memory_mapped_io_port_space,
+    pal_code,
+    persistent,
+    unaccepted,
+    _,
+};
+
 pub const TaskPriorityLevel = enum(usize) {
     application = 4,
     callback = 8,
--- lib/std/os/uefi/table/boot_services.zig
+++ lib/std/os/uefi/table/boot_services.zig
@@ -8,7 +8,7 @@ pub const BootServices = extern struct {
         ) callconv(bits.EFIAPI) void,
 
         AllocatePages: *const fn (
-            type: bits.AllocateType.Enum,
+            type: bits.AllocateType,
             memory_type: bits.MemoryType,
             pages: usize,
             memory: *bits.PhysicalAddress,
@@ -34,7 +34,7 @@ pub const BootServices = extern struct {
         ) callconv(bits.EFIAPI) bits.Status,
 
         CreateEvent: *const fn (
-            type: bits.EventType,
+            type: u32,
             notify_tpl: bits.TaskPriorityLevel,
             notify_function: ?*const bits.EventNotifyFunction,
             notify_context: ?*const anyopaque,
@@ -42,12 +42,12 @@ pub const BootServices = extern struct {
         ) callconv(bits.EFIAPI) bits.Status,
         SetTimer: *const fn (
             event: bits.Event,
-            type: bits.TimerDelay.Enum,
+            type: bits.TimerDelay,
             trigger_time: u64,
         ) callconv(bits.EFIAPI) bits.Status,
         WaitForEvent: *const fn (
             number_of_events: usize,
-            event: [*]const bits.Event,
+            event: [*]const ?bits.Event,
             index: *usize,
         ) callconv(bits.EFIAPI) bits.Status,
         SignalEvent: *const fn (
@@ -232,8 +232,233 @@ pub const BootServices = extern struct {
         };
     };
 
+    pub const TimerKind = union(bits.TimerDelay) {
+        /// Cancel any pending timer event.
+        cancel,
+
+        /// The timer will be triggered periodically at `.periodic * 100ns`
+        /// intervals. The event does not need to be reset after each trigger.
+        periodic: u64,
+
+        /// The timer will be triggered in `.relative * 100ns`.
+        relative: u64,
+    };
+
+    pub const AllocateType = union(bits.AllocateType) {
+        any,
+        max_address: bits.PhysicalAddress,
+        exact_address: bits.PhysicalAddress,
+    };
+
     header: Header,
     interface: Interface,
+
+    pub fn createEvent() void {}
+
+    /// Removes the given event handle from all event groups it belongs to, and
+    /// then closes the event handle. Will remove any associated notifications
+    /// registered with the event.
+    ///
+    /// The event handle may no longer be used after this function is called.
+    pub fn closeEvent(
+        boot_services: *const BootServices,
+        event: bits.Event,
+    ) void {
+        // only documented status returned is .success
+        _ = boot_services.interface.CloseEvent(event);
+    }
+
+    /// Place the supplied event into the signaled state. If the event is
+    /// already in the signaled state then this function will have no effect.
+    ///
+    /// If the event is of type EVT_NOTIFY_SIGNAL, then the event's notification
+    /// function will be scheduled to be invoked at the current TPL.
+    ///
+    /// This may be called from any TPL. And if the event is a member of an
+    /// event group, then all events in the group will be signaled as above.
+    pub fn signalEvent(
+        boot_services: *const BootServices,
+        event: bits.Event,
+    ) void {
+        // only documented status returned is .success
+        _ = boot_services.interface.SignalEvent(event);
+    }
+
+    /// Stops execution until an event is signaled. Must not be used with any
+    /// event of type EVT_NOTIFY_SIGNAL.
+    ///
+    /// Must be called at TPL .application
+    ///
+    /// The events are evaluated in the order they are provided:
+    ///
+    /// 1. If an event is already in the signaled state, then the index of that
+    ///    event is returned.
+    ///
+    /// 2. If an event is not in the signaled state, but does have a
+    ///    notification function, then that function is scheduled to be invoked
+    ///    at the requested TPL. If execution of the notification function
+    ///    causes the event to be signaled, then the index of that event is
+    ///    returned.
+    ///
+    /// 3. Repeat until a signaled event is found or an error occurs.
+    ///
+    /// If this function returns an event index, then the signaled state of the
+    /// event is cleared.
+    pub fn waitForEvents(
+        boot_services: *const BootServices,
+        events: [:null]const ?bits.Event,
+    ) !usize {
+        assert(events.len > 0); // must provide at least one event
+
+        var index: usize = 0;
+        try boot_services.interface.WaitForEvent(events.len, events.ptr, &index).fail();
+        return index;
+    }
+
+    /// Checks whether an event is in the signaled state. Must not be used with
+    /// event of type EVT_NOTIFY_SIGNAL. There are 3 possibilities:
+    ///
+    /// 1. If the event is in the signaled state, then `true` is returned.
+    ///
+    /// 2. If the event is not in the signaled state, and has no notification
+    ///    function then `false` is returned.
+    ///
+    /// 3. If the event is not in the signaled state, but does have a
+    ///    notification function, then that function is scheduled to be invoked
+    ///    at the requested TPL. If execution of the notification function
+    ///    causes the event to be signaled, then `true` is returned.
+    ///
+    /// When this function returns `true`, the signaled state of the event is
+    /// cleared.
+    pub fn checkEvent(
+        boot_services: *const BootServices,
+        event: bits.Event,
+    ) !bool {
+        const status = boot_services.interface.CheckEvent(event);
+        switch (status) {
+            .success => return true,
+            .not_ready => return false,
+            else => |s| return s.fail(),
+        }
+    }
+
+    /// Sets the timer event to trigger at the specified time.
+    ///
+    /// Any previous timer event will be canceled.
+    pub fn setTimer(
+        boot_services: *const BootServices,
+        event: bits.Event,
+        delay: TimerKind,
+    ) !void {
+        const trigger_time = switch (delay) {
+            .cancel => 0,
+            .periodic => delay.periodic,
+            .relative => delay.relative,
+        };
+
+        try boot_services.interface.SetTimer(event, delay, trigger_time).fail();
+    }
+
+    /// Raises a task's priority level and returns its previous level.
+    ///
+    /// The TPL provided **must** be lower than or equal to the current TPL,
+    /// otherwise the system may exhibit indeterminate behavior.
+    pub fn raiseTpl(
+        boot_services: *const BootServices,
+        new_tpl: bits.TaskPriorityLevel,
+    ) bits.TaskPriorityLevel {
+        return boot_services.interface.RaiseTPL(new_tpl);
+    }
+
+    /// Restores a task's priority level to its previous value.
+    ///
+    /// The TPL provided **must** be higher than or equal to the current TPL,
+    /// otherwise the system may exhibit indeterminate behavior.
+    pub fn restoreTpl(
+        boot_services: *const BootServices,
+        old_tpl: bits.TaskPriorityLevel,
+    ) void {
+        boot_services.interface.RestoreTPL(old_tpl);
+    }
+
+    /// Allocates memory pages from the system. The requested chunk of memory
+    /// will be allocated from the firmware's memory map.
+    ///
+    /// In general, UEFI OS loaders and UEFI applications should allocate memory
+    /// of type `.loader_data`.
+    ///
+    /// UEFI Boot Service Drivers **must** allocate memory of type
+    /// `.boot_services_data`
+    ///
+    /// UEFI Runtime Service Drivers **must** allocate memory of type
+    /// `.runtime_services_data` (although such allocations must be made during
+    /// boot services time).
+    /// 
+    /// Should be freed with `freePages`.
+    pub fn allocatePages(
+        boot_services: *const BootServices,
+        allocate_type: AllocateType,
+        memory_type: bits.MemoryType,
+        pages: usize,
+    ) ![]align(4096) u8 {
+        assert(memory_type != .persistent and memory_type != .unaccepted); // these are not allowed
+
+        var memory: bits.PhysicalAddress = switch (allocate_type) {
+            .any => 0,
+            .max_address => |addr| addr,
+            .exact_address => |addr| addr,
+        };
+
+        try boot_services.interface.AllocatePages(allocate_type, memory_type, pages, &memory).fail();
+        const allocation: [*]align(4096) u8 = @ptrFromInt(memory);
+        const length = pages * 4096;
+
+        return allocation[0..length];
+    }
+
+    /// Frees memory pages allocated with `allocatePages`. The memory is
+    /// returned to the system's memory map.
+    pub fn freePages(
+        boot_services: *const BootServices,
+        memory: []align(4096) u8,
+    ) void {
+        const num_pages = @divExact(memory.len, 4096);
+        try boot_services.interface.FreePages(@intFromPtr(memory), num_pages).fail();
+    }
+
+    pub fn getMemoryMap() void {}
+
+    /// Allocates memory from the firmware's memory pool. 
+    ///
+    /// In general, UEFI OS loaders and UEFI applications should allocate memory
+    /// of type `.loader_data`.
+    ///
+    /// UEFI Boot Service Drivers **must** allocate memory of type
+    /// `.boot_services_data`
+    ///
+    /// UEFI Runtime Service Drivers **must** allocate memory of type
+    /// `.runtime_services_data` (although such allocations must be made during
+    /// boot services time).
+    /// 
+    /// Should be freed with `freePool`.
+    pub fn allocatePool(
+        boot_services: *const BootServices,
+        pool_type: bits.MemoryType,
+        size: usize,
+    ) ![]align(8) u8 {
+        var buffer: [*]align(8) u8 = undefined;
+        try boot_services.interface.AllocatePool(pool_type, size, &buffer).fail();
+        return buffer[0..size];
+    }
+
+    /// Frees memory allocated with `allocatePool`. The memory is returned to
+    /// the firmware's memory pool.
+    pub fn freePool(
+        boot_services: *const BootServices,
+        buffer: []align(8) u8,
+    ) void {
+        try boot_services.interface.FreePool(buffer.ptr).fail();
+    }
 };
 
 const AnyInterface = *const anyopaque;
@@ -244,3 +469,6 @@ const Header = @import("header.zig").Header;
 
 const bits = @import("../bits.zig");
 const protocol = @import("../protocol.zig");
+
+const assert = std.debug.assert;
+const std = @import("../../../std.zig");
